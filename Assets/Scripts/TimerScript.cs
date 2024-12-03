f5375b547d3a55a25ab3113062384ebfdefee01a
Laura <kanervalaurajohanna@gmail.com>
2024-12-03 13:34:01 +0200

    Create timer

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+public class TimerScript : MonoBehaviour
+{
+    public float TimeLeft = 30;
+    public bool TimerOn = false;
+    public TextMeshProUGUI timerText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        TimerOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimerOn)
+        {
+            if (TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                updateTimer(TimeLeft);
+            }
+            else
+            {
+                TimeLeft = 0;
+                TimerOn = false;
+            }
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerText.text = string.Format("{00}", seconds);
+    }
+}
