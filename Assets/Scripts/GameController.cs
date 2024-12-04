fe7208455d1f529d9a7301feadfab6a332f5cac4
Laura <kanervalaurajohanna@gmail.com>
2024-12-04 21:03:34 +0200

    Create game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+    private TimerScript timer;
+    public List<Mushroom> mushrooms;
+    public HashSet<Mushroom> currentMushrooms = new HashSet<Mushroom>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < mushrooms.Count; i++)
+        {
+            mushrooms[i].Hide();
+            mushrooms[i].SetIndex(i);
+        }
+        currentMushrooms.Clear();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentMushrooms.Count < 1)
+        {
+            int index = Random.Range(0, mushrooms.Count);
+            if (!currentMushrooms.Contains(mushrooms[index]))
+            {
+                currentMushrooms.Add(mushrooms[index]);
+                mushrooms[index].Activate();
+            }
+        
+        }
+    }
+
+    public void GameOver()
+    {
+        foreach (Mushroom mushroom in mushrooms)
+        {
+            mushroom.StopGame();
+        }
+    }
+}
